cbc34f9f8b6e349ca4b98ab53ecc64df92aa7245
noamgk1 <noamgk1@gmail.com>
2021-10-13 13:56:21 +0300

    targil 0

---
+++ Targil0/Program5409.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void Wellcome5409()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
