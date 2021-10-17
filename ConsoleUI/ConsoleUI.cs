ad9a7b5856012c1ced1b00e833a2284daa10422b
otapiero <oht862@gmail.com>
2021-10-17 10:53:21 +0300

    1.2

--- ConsoleUI/ConsoleUI.cs
+++ ConsoleUI/ConsoleUI.cs
@@ -7,8 +7,8 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            DAL.Foo c1 = new();
-            Console.WriteLine( c1.k);
+           
+            
         }
     }
 }
---
+++ DAL/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct Customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Phone { get; set; }
+            public double Longitude { get; set; }
+            public double Lattitude { get; set;
+            }
+
+        }
+    }
+}
