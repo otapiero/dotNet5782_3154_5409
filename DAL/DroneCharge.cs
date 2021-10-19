f0ae8232351408e53339d19378d6a0faffbcb800
otapiero <oht862@gmail.com>
2021-10-19 11:00:05 +0300

    targil1 1.4 otapiero

---
+++ DAL/DroneCharge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public struct DroneCharge
+    {
+        public int DroneId { get; set; }
+        public int StationId { get; set; }
+    }
+}
