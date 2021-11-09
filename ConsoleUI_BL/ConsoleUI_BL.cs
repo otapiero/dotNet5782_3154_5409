0376cb31819c535d4ca7e9fa5d52d5449fa144a7
otapiero <oht862@gmail.com>
2021-11-09 11:22:47 +0200

    nuthing

---
+++ ConsoleUI_BL/ConsoleUI_BL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleUI_BL
+{
+    class ConsoleUI_BL
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
